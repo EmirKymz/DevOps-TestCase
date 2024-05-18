c17d47251e8409d2c3be473b8f8efa1203811446
Emircan Kaymaz <99013427+EmirKymz@users.noreply.github.com>
2024-05-18 16:22:31 +0300

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,30 @@
-public class HelloWorld {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CreateTestDirectory {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // "/home/test" adında bir klasör oluştur
+        File testDir = new File("/home/test");
+
+        if (!testDir.exists()) {
+            if (testDir.mkdir()) {
+                System.out.println("Test klasörü başarıyla oluşturuldu.");
+            } else {
+                System.out.println("Test klasörü oluşturulamadı.");
+                return;
+            }
+        }
+
+        // "/home/test/readme.txt" dosyasını oluştur
+        File readmeFile = new File(testDir, "readme.txt");
+
+        try (FileWriter writer = new FileWriter(readmeFile)) {
+            // "Hello World" yaz
+            writer.write("Hello World");
+            System.out.println("readme.txt dosyasına 'Hello World' yazıldı.");
+        } catch (IOException e) {
+            System.out.println("Dosya oluşturma/yazma hatası: " + e.getMessage());
+        }
     }
 }
